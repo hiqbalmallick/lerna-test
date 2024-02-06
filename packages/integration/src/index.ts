97e50c22101b7e9e9254a3de8deae9bdb93d06f1
Hamza Iqbal <hiqbalmallick@gmail.com>
2024-02-06 15:59:14 +0800

    deleted integration package

--- packages/integration/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Doctor } from "@test-sdk/doctor";
-import { Patient } from "@test-sdk/patient";
-import { Scheduler } from "@test-sdk/scheduler";
-
-console.log(new Doctor());
-console.log(new Scheduler());
-console.log(new Patient());
