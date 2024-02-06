cb56856e3538d948ae6fda9336343b14d8e2d75f
Hamza Iqbal <hiqbalmallick@gmail.com>
2024-02-06 20:21:05 +0800

    updated package json

---
+++ packages/patient/version-fix.js
@@ -0,0 +1,24 @@
+//Load the library
+const replace = require("replace-in-file");
+const version = require("./package.json").version;
+// path for metro config file
+const path = "lib/patient.js";
+// creating options for editing the file
+const options = [
+  {
+    files: path,
+    from: "__HOST_PACKAGE_VERSION__",
+    to: version,
+  },
+];
+
+try {
+  let results;
+  // looping on each option
+  options.forEach((e) => {
+    results = replace.sync(e);
+    console.log('Replacing "' + e.from + '" by "' + e.to);
+  });
+} catch (error) {
+  console.error("Error occurred:", error);
+}
