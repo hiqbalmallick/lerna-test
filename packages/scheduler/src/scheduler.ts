cc4d42d97893f317592f1165d5dfb900d171850b
Hamza Iqbal <hiqbalmallick@gmail.com>
2024-02-07 11:02:03 +0800

    updated packages

--- packages/scheduler/src/scheduler.ts
+++ packages/scheduler/src/scheduler.ts
@@ -1,6 +1,6 @@
 // scheduler.ts
 export class Scheduler {
-  private sdkVersion: string = '__HOST_PACKAGE_VERSION__';
+  private _sdkVersion: string = '__HOST_PACKAGE_VERSION__';
   get(id) {
     return {};
   }
