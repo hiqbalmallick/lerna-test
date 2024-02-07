7ffea7f2f2e3c19874a5a8c167315790538c6c50
Hamza Iqbal <hiqbalmallick@gmail.com>
2024-02-07 12:22:28 +0800

    update

--- packages/patient/src/patient.ts
+++ packages/patient/src/patient.ts
@@ -1,6 +1,6 @@
 // patient.ts
 export class Patient {
-  private _sdkVersion: string = '__HOST_PACKAGE_VERSION__';
+  private sdkVersion: string = '__HOST_PACKAGE_VERSION__';
   get(id) {
     return {};
   }
